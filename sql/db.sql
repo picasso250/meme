fb1e8e9c2d149932dcd0f6cfb9d8ef9904a12530
xiaochi <cumt.xiaochi@gmail.com>
2014-02-07 13:14:32 +0800

    add some table

--- sql/db.sql
+++ sql/db.sql
@@ -4,6 +4,19 @@ CREATE DATABASE IF NOT EXISTS bbs CHARACTER SET UTF8 COLLATE utf8_general_ci;
 
 USE bbs;
 
+-- ----------------------------
+-- Table structure for article
+-- ----------------------------
+DROP TABLE IF EXISTS `article`;
+CREATE TABLE `article` (
+  `id` int(11) NOT NULL,
+  `text` text,
+  `create_time` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP,
+  `last_editor` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
 -- 节点，最基本的单位（节点一旦创建，就不可更改，现在，也不能分裂了）
 CREATE TABLE IF NOT EXISTS `node`
 (
@@ -18,14 +31,37 @@ CREATE TABLE IF NOT EXISTS `node`
 CREATE TABLE IF NOT EXISTS `topic`
 (
     `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
-    `origin` INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT '起源',
-    `editor` INT(10) UNSIGNED NOT NULL COMMENT '编辑者',
-    `is_leaf` TINYINT(1) UNSIGNED NOT NULL DEFAULT '1' COMMENT '编辑者',
+    `creator` INT(10) UNSIGNED NOT NULL COMMENT '编辑者',
     `title` TEXT NOT NULL COMMENT '标题', 
-    `nodes` TEXT NOT NULL COMMENT '内容', 
     `time` DATETIME NOT NULL COMMENT '最后被 touch 的时间',
     PRIMARY KEY(id)
 ) ENGINE=MyISAM;
+
+-- ----------------------------
+-- Table structure for reply
+-- ----------------------------
+DROP TABLE IF EXISTS `reply`;
+CREATE TABLE `reply` (
+  `id` int(11) NOT NULL,
+  `text` text,
+  `author` int(10) unsigned DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Table structure for tiezi
+-- ----------------------------
+DROP TABLE IF EXISTS `tiezi`;
+CREATE TABLE `tiezi` (
+  `id` int(11) NOT NULL,
+  `author` int(10) unsigned DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `text` text,
+  `create_time` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 
 -- 用户，
 CREATE TABLE IF NOT EXISTS `user`
@@ -34,6 +70,8 @@ CREATE TABLE IF NOT EXISTS `user`
     `name` CHAR(20) NOT NULL COMMENT '用户名',
     `message` TEXT COMMENT '用户的签名',
     `create_time` DATETIME NOT NULL COMMENT '注册时间',
+    `last_login_time` datetime DEFAULT NULL,
+    `password` varchar(255) DEFAULT NULL,
     PRIMARY KEY(id)
 ) ENGINE=MyISAM;
 
