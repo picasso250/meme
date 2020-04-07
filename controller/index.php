3da4bc8818dfacd08925b1f1a0c72e887d3debd1
xiaochi <cumt.xiaochi@gmail.com>
2020-04-07 21:39:42 +0800

    as

--- controller/index.php
+++ controller/index.php
@@ -1,18 +1,23 @@
 <?php
 
 use model\Topic;
+use model\User;
 /**
  * @file    index
  * @author  ryan <cumt.xiaochi@gmail.com>
  * @created Jun 27, 2012 6:24:01 PM
  */
 
-require_once Pf::lib('Paginate');
 $per_page = 100;
-$id = req('id')?:1;
-$n=$per_page+1;
-$topics = Topic::fetchAll("SELECT * from topic where id>=? limit $n",[$id]);
+$id = req('id') ?: 1;
+$n = $per_page + 1;
+$topics = Topic::fetchAll("SELECT * from topic where id>=? order by id DESC limit $n", [$id]);
+$uids = array_map(function ($e) {
+    return $e->editor;
+}, $topics);
+if ($uids) {
+    $users = User::findByIds($uids);
+}
 
 $view .= '?master';
 $page['scripts'][] = 'widget';
-
